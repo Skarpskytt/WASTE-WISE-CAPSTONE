ac3446127582ba02868afceb8f8a347716e6fa41
JJ <joshuabchua11@gmail.com>
2024-11-21 02:01:09 +0800

    Update

---
+++ assets/style/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Helvetica', 'Arial', 'sans-serif'],
+        serif: ['Georgia', 'serif'],
+        mono: ['Menlo', 'Monaco', 'Courier New', 'monospace'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
